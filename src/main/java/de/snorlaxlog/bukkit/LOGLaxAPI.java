6e064d84f25e01ed17f4897a70d798ed6790115c
Jotrorox <jotrorox@gmail.com>
2024-02-03 15:56:02 +0100

    Remove unnecessary import statement and add a HashMap to store CachedPlayer objects
    Subject: Remove import statement and add HashMap
    
    Remove unnecessary import statement and add a HashMap to store CachedPlayer objects.

--- src/main/java/de/snorlaxlog/bukkit/LOGLaxAPI.java
+++ src/main/java/de/snorlaxlog/bukkit/LOGLaxAPI.java
@@ -1,6 +1,5 @@
 package de.snorlaxlog.bukkit;
 
-import de.snorlaxlog.bukkit.channels.PluginChannelMessage;
 import de.snorlaxlog.bukkit.channels.PluginChannelMessageListener;
 import de.snorlaxlog.bukkit.commands.abrax.WarpUICommand;
 import de.snorlaxlog.bukkit.interfaces.CachedPlayer;
@@ -19,6 +18,7 @@ public class LOGLaxAPI extends JavaPlugin {
     private static LOGLaxAPI instance;
     public de.snorlaxlog.bukkit.mysql.MySQL mysql;
     private HashMap<String, CachedPlayer> allCachedPlayersByName;
+
     @Override
     public void onLoad() {
         instance = this;
@@ -38,30 +38,30 @@ public class LOGLaxAPI extends JavaPlugin {
         FileManager.deleteFiles();
     }
 
-    public void CommandRegistration(){
+    public void CommandRegistration() {
         getCommand("warpui").setExecutor(new WarpUICommand());
     }
 
-    public void ListenerRegistration(){
+    public void ListenerRegistration() {
         getServer().getPluginManager().registerEvents(new InventoryManagerClickListener(), this);
         getServer().getMessenger().registerIncomingPluginChannel(this, "BungeeCord", new PluginChannelMessageListener());
     }
 
-    public void startEssentials(){
+    public void startEssentials() {
         FileManager.createFiles();
         this.loadMySQL();
         SQLManager.initializeDatabase();
     }
 
-    public void onStartUp(){
+    public void onStartUp() {
         Bukkit.getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
     }
 
-    public static void logMessage(Level level, String message){
+    public static void logMessage(Level level, String message) {
         Bukkit.getLogger().log(level, message);
     }
 
-    public void loadMySQL(){
+    public void loadMySQL() {
 
         String host = FileManager.getHost();
         int port = FileManager.getPort();
@@ -70,7 +70,7 @@ public class LOGLaxAPI extends JavaPlugin {
         String password = FileManager.getPassword();
 
         mysql = new MySQL(host, port, database, user, password);
-        if (mysql.getConnection() == null || !mysql.hasConnection()){
+        if (mysql.getConnection() == null || !mysql.hasConnection()) {
             logMessage(Level.OFF, "Disabling SnorlaxLOG-API cause the MySQL-Connection has been lost!");
             this.onDisable();
         }
