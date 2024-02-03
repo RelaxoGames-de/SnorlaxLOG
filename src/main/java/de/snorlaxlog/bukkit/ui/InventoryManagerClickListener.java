0c2420fa686c04af0c071cae1d3d6b007ba055c1
DevTex <stiers609@gmail.com>
2024-02-03 02:24:39 +0100

    Changes in:  - InventoryManagerClickListener.java
    Fixed: Read: https://github.com/RelaxoGames-de/SnorlaxLOG/issues/7 for more Informations
    
    Pluginversion: inDev-1.9.3

--- src/main/java/de/snorlaxlog/bukkit/ui/InventoryManagerClickListener.java
+++ src/main/java/de/snorlaxlog/bukkit/ui/InventoryManagerClickListener.java
@@ -1,20 +1,14 @@
 package de.snorlaxlog.bukkit.ui;
 
-import de.snorlaxlog.bukkit.LOGLaxAPI;
 import de.snorlaxlog.bukkit.commands.abrax.AbraxUIClickEventHandler;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-
 public class InventoryManagerClickListener implements Listener {
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getCurrentItem().getItemMeta() == null || event.getCurrentItem().getItemMeta().getLocalizedName() == null)return;
         if (event.getCurrentItem().getItemMeta().getLocalizedName().startsWith("snorlaxlog.ui.inventorymanager.warpui.")) AbraxUIClickEventHandler.handleClick(event);
     }
 }
