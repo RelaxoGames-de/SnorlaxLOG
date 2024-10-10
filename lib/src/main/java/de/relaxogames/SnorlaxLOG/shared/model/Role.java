091cd88d77b055989d90284a216ff8f8173c6ba4
Johannes (Jotrorox) Müller <mail@jotrorox.com>
2024-10-10 14:04:52 +0200

    Add serialization and deserialization methods for Role class

--- lib/src/main/java/de/relaxogames/SnorlaxLOG/shared/model/Role.java
+++ lib/src/main/java/de/relaxogames/SnorlaxLOG/shared/model/Role.java
@@ -1,5 +1,8 @@
 package de.relaxogames.SnorlaxLOG.shared.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * This class is used to define the roles of the users.
  * A role is later on used to define the permissions of the user
@@ -39,5 +42,44 @@ public class Role {
      */
     public String getName() {
         return Role.NAME;
+    }
+
+    /**
+     * A simple function to deserialize a List of Roles
+     * in the db into a List of Role Classes
+     * 
+     * @param roleString the Roles as a String
+     * @return The deserialized Roles
+     * 
+     * @see Role
+     * @since 2.0
+     * @version 1.0
+     */
+    public static List<Role> fromString(String roleString) {
+        String[] rStringArr = roleString.split(";");
+        List<Role> roles = new ArrayList<>();
+
+        for (String rString : rStringArr) roles.add(new Role(rString));
+
+        return roles;
+    }
+
+    /**
+     * A simple function to serialize a List of Roles
+     * in the db into a String
+     * 
+     * @param roles the Roles as a List
+     * @return The serialized Roles
+     * 
+     * @see Role
+     * @since 2.0
+     * @version 1.0
+     */
+    public static String toString(List<Role> roles) {
+        StringBuilder sb = new StringBuilder();
+
+        for (Role role : roles) sb.append(role.getName()).append(";");
+
+        return sb.toString();
     }
 }
